3788f971c51c3a4f1bce9151e9e024fff4e7075d
James <atkisson.james@gmail.com>
2020-07-18 13:31:58 -0500

    Update to complete challenge in part 2 of tutorial

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET users listing. */
 router.get('/:Title', function(req, res, next) {
-  try {res.render('index', { title: req.params.Title });} 
-  catch (error) {
-    console.log(error);
-    res.render('index', { title: 'None Received'});
-  }
+  res.send('You are so ' + req.params.Title)
 });
 
 module.exports = router;
