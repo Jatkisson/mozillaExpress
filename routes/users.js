5a32cae8703e5bfb8b7a5e96044172b10eb55ec5
James <atkisson.james@gmail.com>
2020-07-18 09:55:42 -0500

    Added some modifications to the users route

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:Title', function(req, res, next) {
+  try {res.render('index', { title: req.params.Title });} 
+  catch (error) {
+    console.log(error);
+    res.render('index', { title: 'None Received'});
+  }
 });
 
 module.exports = router;
